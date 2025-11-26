00268e544bef1d5bb35dad24b7a3c55dcf81a2aa
Black4445e <brunomiguelsilvagomes19@gmail.com>
2025-11-26 02:07:39 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1269,7 +1269,7 @@ async def MaiiiinE():
     )
     global loop, key, iv, region
     
-    Uid , Pw = '4307598022','BY_MERCY-WBBIHHNVK-HACKED'
+    Uid , Pw = '4309458151','BY_MERCY-EDGMAAPJF-HACKED'
 
     open_id , access_token = await GeNeRaTeAccEss(Uid , Pw)
     if not open_id or not access_token: 
